3c3a087265fb35babf243a66de15d02e0cda2235
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-01-17 16:26:55 +0100

    Added cards shuffle on click
    When a card is clicked, cards are shuffled and re-rendered

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -16,14 +16,33 @@ export default function Cards() {
         fetchData();
     }, []);
 
+    const shuffleCards = (array) => {
+        let copy = array.slice(0);
+        for (var i = copy.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = copy[i];
+            copy[i] = copy[j];
+            copy[j] = temp;
+        }
+        return copy
+    } 
+
+    const clickHandle = () => {
+        setPokemonData(shuffleCards(pokemonData));
+    }
+
 if (pokemonData[0])    return (
         <div>
           {pokemonData.map((pokemon, index) => (
             <div key={index}>
               <p>{pokemon.name}</p>
-              <img src={pokemon.img} alt={pokemon.name} />
+              <img src={pokemon.img} alt={pokemon.name} onClick={clickHandle} />
             </div>
           ))}
         </div>
       )
+
+    return (
+        <div>Loading</div>
+    )
           }
